3375aee0aafcab6fcb1c5be13b5971b079b27d71
bishunmo <bishunmo@gmail.com>
2017-07-12 11:29:39 +0300

    block class added

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -105,6 +105,15 @@ public class GameTetris {
     }
 
     class Figure {
+        private ArrayList<Block> figure = new ArrayList<Block>();
+        private int[][] shape = new int[4][4];
+        private int type, size, color;
+        private int x = 3, y = 0;
+
+        Figure() {
+            type = random.nextInt(SHAPES.length);
+        }
+
         boolean isTouchGround() {
             return false;
         }
@@ -135,7 +144,28 @@ public class GameTetris {
     }
 
     class Block {
+        private int x,y;
 
+        public Block(int x, int y) {
+            setX(x);
+            setY(y);
+        }
+
+        void setX(int x) {
+            this.x = x;
+        }
+
+        void setY(int y) {
+            this.y = y;
+        }
+
+        int getX() {return x;}
+        int getY() {return y;}
+
+        void paint(Graphics g, int color){
+            g.setColor(new Color(color));
+            g.drawRoundRect(x * BLOCK_SIZE + 1, y * BLOCK_SIZE+1, BLOCK_SIZE - 2, BLOCK_SIZE - 2, ARC_RADIUS, ARC_RADIUS);
+        }
     }
 
     public class Canvas extends JPanel {
